9ffb26415e4d30e8ee300753e6f99f73683535c1
Kirill Scherbakoff <elkirrs@gmail.com>
2020-03-08 18:25:17 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if  (str.length % 2 == 1) {
+        return false;
+    }
+
+    let brackets = strIntoArr(bracketsConfig);
+    let len = lengthStr(str, brackets);
+    return checkBool(len);
+};
+
+function strIntoArr(bracketsConfig) {
+    let newBrackets = [];
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        newBrackets.push(bracketsConfig[i][0] + bracketsConfig[i][1])
+    }
+    return newBrackets;
+}
+
+function lengthStr(str, newBrackets) {
+    let len = str.length;
+    for (let i = 0; i < len; i++) {
+        for (let t = 0; t < newBrackets.length; t++) {
+            str = str.replace(newBrackets[t], '')
+        }
+    }
+    return str.length;
+}
+
+function checkBool(number) {
+    if (number / 2 == 0) {
+        return true;
+    }
+    return false;
 }
